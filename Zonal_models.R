6ba4a2fa9e52f2fae1fc7ed588769085c99271a8
Esquivel <A.ESQUIVEL@cgiar.org>
2018-04-05 16:14:20 -0500

    Change Kernel

--- Zonal_models.R
+++ Zonal_models.R
@@ -26,9 +26,11 @@ modelsPath <- paste0(rootPath, '3_monthly_climate_variability/Spatial_models/')
 shpPath <- paste0(rootPath, '0_general_inputs/shp/')
 
 
+
 # =-=-=-= Preliminar parameters 
-crop <- 'Wheat'
-seasonCrop <- 'wheat_spring'
+crop <- 'Maize'
+seasonCrop <- 'maize_major'
+
 
 
 i <- 1
@@ -38,18 +40,34 @@ print(paste0('Crop: ', crop, ' --- Season: ', seasonCrop, ' --- Variable: ', nam
 
 
 
+
+# Temporarily st_read(dsn = paste0(shpPath, 'mapa_mundi.shp'))
+shp_colombia <- st_read(dsn = paste0(shpPath, 'all_countries.shp')) %>%
+  as('Spatial') %>% crop(extent(-180, 180, -50, 50)) 
+
+# shp_colombia$UNREG2 %>% unique %>% as.tibble() %>% View
+# adm <- shp_colombia[shp_colombia$NAME %in%  c('Mexico'),]
+adm <- shp_colombia[shp_colombia$UNREG1 %in%  c('Northern America'),]
+
+
+plot(adm)
+ext1 <- c(extent(adm)[1:2], extent(adm)[3], 40)
+adm <- crop(adm, ext1)
+
+
+
 # Other package for explore: library(spgwr)
 
 # =-=- on this part we need to use the calendar for to determine what years to use  in the modelation
 
 
-iizumi <- list.files(paste0(IizumiPath, seasonCrop), pattern = 'gap.tif$', full.names = TRUE)[i] %>% 
+iizumi <- list.files(paste0(IizumiPath, seasonCrop), pattern = 'gap.tif$', full.names = TRUE)[30] %>% 
   stack() %>% 
   crop(extent(-180, 180, -50, 50))
 
 
 
-climate <- stack(x = list.files(paste0(climatePath, crop), full.names=T)[i] , bands= i) %>% 
+climate <- stack(x = list.files(paste0(climatePath, crop), full.names=T)[i] , bands= 30) %>% 
   flip(., direction = 2) %>%
   t()
 
@@ -58,66 +76,37 @@ crs(climate) <- "+proj=longlat +datum=WGS84 +no_defs +ellps=WGS84 +towgs84=0,0,0
 
 
 test <- rasterToPoints(climate)
-
-
 test %>% summary
-
-
 sum(test[,3] == -999)
-
-
-
 
 
 
 
 # =-=-=-=-=-=- stack(climate, iizumi) %>% plot
 
-
-
-# Temporarily 
-shp_colombia <- st_read(dsn = paste0(shpPath, 'mapa_mundi.shp')) %>%
-  as('Spatial') %>% crop(extent(-180, 180, -50, 50)) 
-
-# shp_colombia %>% data.frame() %>% filter(REGION == 'Sub Saharan Africa')
-
-adm <- shp_colombia[shp_colombia$REGION %in% c( 'Sub Saharan Africa'),] 
-
-#plot(adm)
-
-shp_colombia$REGION %>% unique %>% as.tibble()
-
-
-
-
-# library(maptools)
-# adm <- raster::getData('GADM', country='AUS', level=1)
-# ccodes()
-
-
-#plot(iizumi) 
-#plot(adm , add = TRUE)
-
-
-Rp2 <- stack(climate, iizumi) %>% crop(., adm) %>% rasterToPoints() # 
+Rp2 <- stack(climate, iizumi) %>% crop(., adm)  %>% mask(., adm) %>% rasterToPoints()  # 
 Rp2 <- Rp2[-which(Rp2[,4] %>% is.na(.)) , ] %>% data.frame()
-# Rp2 <- Rp2[-which(Rp2[,3] == -999) , ] %>% data.frame()
+Rp2 <- Rp2[-which(Rp2[,3] == -999) , ] %>% data.frame()
 
 Rp2 <- SpatialPointsDataFrame(coords = Rp2[,1:2], data = Rp2[,3:4])
+
 
 
 # =-=-=-= Basic GW regression
 
 linmod <- lm(yield_1981_gap~layer,data=Rp2) # Store the regression model to use in a plot later
 summary(linmod)$adj.r.squared
-
+#  0.004196895
 
 
 
 library(mgcv)
 gam_mod <- gam(yield_1981_gap~s( layer, fx = TRUE),data=Rp2) # Store the regression model to use in a plot later
 summary(gam_mod)$dev.expl  
+#0.1011333
 
+
+(76723 / 540900) * 100
 
 
 
@@ -133,14 +122,14 @@ DM <- gw.dist(dp.locat=coordinates(Rp2), p =  3)
 
 
 # conectar con el resto
-bw.gwr.1 <- bw.gwr(yield_1981_gap ~ layer,
-                   data = Rp2, approach = "AICc",
-                   kernel = "gaussian", dMat = DM, adaptive = TRUE)
+bw.gwr.1 <- bw.gwr(yield_2010_gap ~ layer,
+                   data = Rp2, 
+                   kernel = "bisquare", dMat = DM)
 print(bw.gwr.1)
 # 20
-
-system.time(gwr.res <- gwr.basic(  formula = yield_1981_gap~layer, data = Rp2, bw=50, dMat=DM,  kernel='gaussian'))
-# 15.55297 min 
+#bw.gwr.1 <- 25
+system.time(gwr.res <- gwr.basic(  formula = yield_2010_gap~layer, data = Rp2,
+                                   bw = bw.gwr.1, dMat = DM,  kernel = "bisquare"))
 
 
 gwr.res$GW.diagnostic$gwR2.adj
@@ -154,9 +143,6 @@ Coords <- coordinates(gwr.res$SDF) %>%
 
 
 df_data <- bind_cols(as_tibble(gwr.res$SDF), Coords)
-
-
-
 
 rasterize_masa <-  function(var, data, raster){
   points <- data %>%
@@ -176,15 +162,15 @@ p <- rasterize_masa('residual', df_data , iizumi %>% crop(., adm))
 
 #plot(p)
 #plot(adm, add = T)
-
-
-
-
 ggplot(df_data, aes(long, lat, fill = Local_R2)) + 
-  geom_tile() +  
+  geom_tile() +   
+  scale_fill_gradientn(colours = rev(terrain.colors(10)),  limits=c(0,1)) +
   geom_polygon(data = adm, aes(x=long, y = lat, group = group), color = "gray", fill=NA) + 
+  #geom_polygon(data = shp_colombia, aes(x=long, y = lat, group = group), color = "black", fill=NA) + 
   theme_bw() + coord_fixed()
 
+
+write.csv(df_data, paste0(modelsPath, 'USA_S.csv'))
 
 
 

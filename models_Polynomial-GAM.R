a37c8efea38ca443c4effa5f3e5d7f4af55f5e3e
jeikosd <jeison.mesa@correounivalle.edu.co>
2018-04-19 12:27:28 -0500

    addin new extraction of climate with gaps

--- models_Polynomial-GAM.R
+++ models_Polynomial-GAM.R
@@ -1,3 +1,164 @@
+
+############### modelos diferentes 
+library(tidyverse)
+library(raster)
+library(rgdal)
+library(ncdf4)
+library(rgeos)
+library(sf)
+library(mgcv)
+
+
+rootPath <- '//dapadfs/Workspace_cluster_9/AgMetGaps/'
+calendarPath <-  paste0(rootPath, '3_monthly_climate_variability/katia_calendar/')
+climatePath <- paste0(rootPath, '3_monthly_climate_variability/katia_climate/') 
+IizumiPath <- paste0(rootPath, '1_crop_gaps/iizumi_processed/')
+# modelsPath <- paste0(rootPath, '3_monthly_climate_variability/models_02/')
+# modelPolyPath <- paste0(modelsPath, 'polynomial/')
+# modelGAMpath <- paste0(modelsPath, 'GAM/')
+shpPath <- paste0(rootPath, '0_general_inputs/shp/')
+
+crop <- 'Maize'
+seasonCrop <- 'maize_major'
+
+# phenology <- c('Planting', 'Flowering', 'Harvest')
+
+## loading calendar map
+calendar <- list.files(paste0(calendarPath, crop), pattern = 'Int.tif$', full.names = TRUE) %>%
+  raster::stack() %>% 
+  raster::crop(extent(-180, 180, -50, 50))
+## loading yield gap
+gap_iizumi <- list.files(paste0(IizumiPath, seasonCrop), pattern = 'gap.tif$', full.names = TRUE) %>% 
+  raster::stack() %>% 
+  raster::crop(extent(-180, 180, -50, 50))
+
+
+
+# knowing what is the number of year, we know that iizumi information it is from 1981 to 2011
+number_years <- length(names(gap_iizumi))
+
+# extract coordinates from iizumi yield gap
+gaps_sf <- rasterToPoints(gap_iizumi)%>%
+  tbl_df() %>%
+  dplyr::rename(lat = y, long = x) %>%
+  st_as_sf(coords = c("long", "lat"))
+
+
+# values_gap <- rasterToPoints(gap_iizumi)[, 3] %>%
+#   tbl_df() %>%
+#   rename(gap = value)
+## i = 1
+
+## function to extract the trimester variable
+
+extract_trimestre <- function(x){
+  # list.files(paste0(climatePath, crop), full.names=T)
+  
+  x %>%
+    basename() %>% 
+    stringr::str_replace(pattern = "[0-9]+.nc$", replacement = '')
+}
+
+## function to read n bands from a raster
+
+read_bands <- function(x, n_bands){
+  
+  stack(x, bands = 1:n_bands) %>%
+    flip(direction = 2) %>%
+    t()
+  
+}
+
+trimester_files <- list.files(paste0(climatePath, crop), full.names = T)
+
+## aplicar plan future
+library(future)
+library(future.apply)
+library(velox)
+plan(sequential)
+plan(future::cluster, workers = 28)
+options(future.globals.maxSize= 8912896000)
+
+## reading number_years of band in trimester files make for Katia (average for each trimester in planting flowering and harvesting)
+stk_vx <- future.apply::future_lapply(X = trimester_files, FUN = read_bands, number_years) %>%
+  future.apply::future_lapply(FUN = velox) 
+
+## es para averiguar que trimestre es
+
+type <- list.files(paste0(climatePath, crop), full.names = T) %>%
+  data_frame(path = .) %>%
+  mutate(type = basename(path),
+         trimestre = extract_trimestre(path)) %>%
+  dplyr::select(trimestre, everything()) %>%
+  pull(trimestre)
+
+
+## crear funcion para extraer 
+
+date_raster <- 1981:2011
+# values_trimestre(stk_vx[[1]], type[1], spatial_points, date_raster)
+values_trimestre <- function(stk_vx, variable, spatial_points, date_raster){
+  
+  # stk_vx[[1]]
+  # variable <- type[1]
+  # spatial_points <- gaps_sf
+  
+  values <-  stk_vx$extract_points(spatial_points) %>%
+    tbl_df() %>%
+    purrr::set_names(date_raster) 
+  # mutate(id = 1:nrow(.))
+  
+  
+  ## quiza meter estos objetos como inputs para no estarlos creando para cada variable
+  coords <- spatial_points %>%
+    st_coordinates() %>%
+    as_tibble() %>%
+    dplyr::select(X, Y) %>%
+    rename(long = X, lat = Y)
+  
+  # gther_spatial_points <- spatial_points %>%
+  # st_set_geometry(NULL) %>%
+  # gather(year, gap)
+  
+  #######
+  
+  
+  climate_gap <- bind_cols(coords, values) %>%
+    # rename_at(vars(contains('V')), funs(c(!!date))) %>%
+    mutate(id = 1:nrow(.)) %>%
+    gather(year, !!variable, -lat, -long, -id)
+  # bind_cols(gther_spatial_points) %>%
+  # dplyr::select(-year1)
+  # group_by(ID) %>%
+  # nest(-id)
+  
+  
+  
+  return(climate_gap)
+  
+  rm(coords)
+  # rm(gther_spatial_points)
+}
+
+climate <- purrr::map2(.x = stk_vx, .y = type, .f = ~future(values_trimestre(.x, .y, spatial_points, date_raster))) %>%
+  future::values()
+## id, lat, long, climate, year, trimestre, value
+
+climate <- climate %>%
+  purrr::reduce(left_join, by = c('id', 'lat', 'long', 'year'))
+
+## agregar el gap antes de hacer el nest
+
+gther_spatial_points <- gaps_sf %>%
+  st_set_geometry(NULL) %>%
+  mutate(id = 1:nrow(.)) %>%
+  gather(year, gap, -id)
+
+climate <- climate %>%
+  nest(-id)
+
+
+
 # Polynomial models in this case, they are linear regression models with quadratic terms in the variables:
 # y = b0 + b1 * x1 + b2 * (x2)^2
 
